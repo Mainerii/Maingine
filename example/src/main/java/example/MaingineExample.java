7104efd8f95fccad52a4a785573e54a5cbaf2226
Mainerii <koskinen.joosua@gmail.com>
2019-07-10 18:06:33 +0300

    Add example project
    There should be example project. It can be used
    to test the new features and show how to use them.

---
+++ example/src/main/java/example/MaingineExample.java
@@ -0,0 +1,13 @@
+package example;
+
+import maingine.Maingine;
+
+public class MaingineExample {
+
+	public static void main(String[] args) {
+
+		System.out.println(new Maingine().getGreeting());
+
+	}
+
+}
